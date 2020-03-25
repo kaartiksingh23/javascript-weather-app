8a9821e4d67e2060d216cb8cbb4d797799032d9b
Kaartik Singh <kaartiksingh23@gmail.com>
2020-03-25 15:06:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// [START gae_node_request_example]
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname));
-
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
