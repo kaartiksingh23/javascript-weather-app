cc68ef79271fb5c153b113ea9ec2d291a79c1778
Kaartik Singh <kaartiksingh23@gmail.com>
2020-03-25 14:28:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: __dirname });
-});
+app.use(express.static(__dirname));
 
 // Start the server
 const PORT = process.env.PORT || 8080;
